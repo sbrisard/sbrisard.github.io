971b3c84dc5aba5c652d257cd6394bc3f5ac2465
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2013-12-06 08:04:04 +0100

    In file conf.py   - changed Sphinx theme   - do not use permalinks for sections, as they cause problems when pasting     articles in Blogger.

--- source/conf.py
+++ source/conf.py
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -133,6 +133,8 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+
+html_add_permalinks = None
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
